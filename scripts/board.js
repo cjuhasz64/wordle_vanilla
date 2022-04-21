d18f45485000ade80eb606cccd3ff7524de4caa5
cjuhasz64 <54127982+cjuhasz64@users.noreply.github.com>
2022-04-21 18:58:50 +1000

    slight progress

--- scripts/board.js
+++ scripts/board.js
@@ -1,20 +1,82 @@
-let board = "<table>";
 
-let rowCount = 6;
-let wordLength = 5;
 
+
+
+function createBoard(wordLength, noOfGuesses, location)
+{
+
+    for (var i = 0; i < noOfGuesses; i++)
+    {   
+        for (var j = 0; j < wordLength; j++)
+        {
+            let cell = document.createElement("div");
+
+            cell.id = `c${i}${j}`; 
+            cell.innerHTML = cell.id; 
+            
+            location.appendChild(cell).className = "grid-item";
+
+        }
+    }
+
+
+    location.style.gridTemplateColumns = `repeat(${wordLength},auto)`;
+
+}
+
+
+function setHeight(boardId)
+{
+    $(boardId).find('div').each(function(){
+        var innerDivId = $(this).attr('id');
+        document.getElementById("board2").innerHTML = "23";
+    });
+    
+}
+
+createBoard(5,6,document.getElementById('board'));
+
+setHeight("board")
+
+
+
+
+/*
+document.getElementById("board").innerHTML = board;
+
+let x = "";
 
 for (let i = 0; i < rowCount; i++)
 {
-    board += '<tr id="r' + i + '">';
+   
     for (let j = 0; j < wordLength; j++)
     {
-        board += '<td id="c' + i + j + '"></td>';
-
+        //document.getElementById("c00").style.height = document.getElementById("c00").clientWidth + "px";
+        document.getElementById("c00").style.height = "4px";    
     }
-    board += '</tr>';
-}
+
     
+    
+}
+
+*/
 
 
-document.getElementById("board").innerHTML = board;
+
+
+
+
+
+
+
+
+
+
+//`"c${i}${j}"`;
+
+
+//document.getElementById("lol").innerHTML += 
+
+
+
+//style=" height:16px"
